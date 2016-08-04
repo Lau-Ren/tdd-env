339487abdf08ced88537fc0b625bd6620360ca02
lau-ren <colls.lauren@gmail.com>
2016-08-05 09:45:45 +1200

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+import test from 'tape'
+
+
+
+
+
+
+
+test('-TESTS-', function (t) {
+
+  // t.ok("ok")
+  // t.notEqual(actual, expected, 'not equal');
+  // t.equal(actual, expected, 'equal');
+  t.end()
+
+   
+});
